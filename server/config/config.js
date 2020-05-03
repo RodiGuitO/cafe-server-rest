be08987abef94a966831106bc744f8a13ff6757c
Rodri Ivan <rodrigodelarreceha@gmail.com>
2020-05-03 13:18:36 -0300

    Beta cafe-server-rest

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,14 @@
 // configuracion de puertos
 // @ts-ignore
-// process.env.PORT = process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 3000;
 
 // verifico base de datos: dev = desarrollo
 const mongodb = process.env.NODE_ENV || 'dev';
 
 const url_db = (mongodb === 'dev') ?
     'mongodb://localhost:27017/CafeDB' :
-    'mongodb+srv://admin-cafe:b2MMfLhwFGbqcD9@cafe-yd94t.mongodb.net/CafeDB';
+    'mongodb+srv://admin-cafe:b2MMfLhwFGbqcD9@cafe-yd94t.mongodb.net/CafeDB?retryWrites=true&w=majority';
+// no va -> 'mongodb+srv://admin-cafe:b2MMfLhwFGbqcD9@cafe-yd94t.mongodb.net/CafeDB';
 
 process.env.URL = url_db;
 
