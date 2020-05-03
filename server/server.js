4ec7fc9a7982e4a16f2f473924a08bbddb9c985e
Rodri Ivan <rodrigodelarreceha@gmail.com>
2020-05-03 15:17:02 -0300

    Beta cafe-server-rest

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+
+// @ts-ignore : Config global conexion
+require('./config/config');
+
+let usuario = require('./routes/usuario');
+
+// para uso x-www-form-encoded paso de parametros por html
+const bp = require('body-parser');
+
+// parse application/x-www-form-urlencoded : Middleware
+app.use(bp.urlencoded({ extended: false }));
+
+// parse application/json : Middleware
+app.use(bp.json());
+
+app.use(usuario);
+
+// const options = (require('./config/config').mongodb === 'env') ? { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true } : {};
+
+mongoose.connect(process.env.URL,
+    (error, resp) => {
+        if (error) {
+            // throw error(error);
+            return console.log('Error de conexion a BD CafeDB : ', error);
+        }
+        console.log('Base de datos CafeDB inicializada', resp.client.s.url);
+    });
+
+app.listen(process.env.PORT, () => {
+    console.log('Server: http://localhost:', process.env.PORT);
+});
